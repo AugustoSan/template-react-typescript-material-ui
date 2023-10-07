c8560ce8ed90167d003c25f6bd6d1fe17e61dbdd
Augusto Sanchez Julian <julian.a@dicio.ai>
2023-10-07 13:58:18 -0600

    Se descomentan lineas para persist

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,28 +1,28 @@
 import { configureStore } from '@reduxjs/toolkit';
-// import { persistStore, persistReducer } from 'redux-persist';
-// import storage from 'redux-persist/lib/storage';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import authReducer from './slices/auth';
 
-// const persistAuthConfig = {
-//   key: 'auth',
-//   // storage,
-//   whitelist: ['accessToken'], // lo que queremos que se guarden
-//   blacklist: ['loQueNoQuiero'] // Lo que no quiero que se guarde
-// };
+const persistAuthConfig = {
+  key: 'auth',
+  storage,
+  whitelist: ['accessToken'], // lo que queremos que se guarden
+  blacklist: ['loQueNoQuiero'] // Lo que no quiero que se guarde
+};
 
 const store = configureStore({
   reducer: {
-    // auth: persistReducer<ReturnType<typeof authReducer>>(
-    //   persistAuthConfig,
-    //   authReducer
-    // ),
-    auth: authReducer
-  }
-  // middleware: (defaultMiddleware) =>
-  //   defaultMiddleware({
-  //     serializableCheck: false
-  //   })
+    auth: persistReducer<ReturnType<typeof authReducer>>(
+      persistAuthConfig,
+      authReducer
+    )
+    // auth: authReducer
+  },
+  middleware: (defaultMiddleware) =>
+    defaultMiddleware({
+      serializableCheck: false
+    })
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
@@ -30,6 +30,6 @@ export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
 
-// export const persistor = persistStore(store);
+export const persistor = persistStore(store);
 
 export default store;
